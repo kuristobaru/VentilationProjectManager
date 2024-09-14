b3288fde2288ddb6e0aaf49da34a9aedde537b47
Javier <javiersotosm7@gmail.com>
2024-09-14 12:10:05 -0400

    feature import project

--- backend/src/controllers/project.controllers.js
+++ backend/src/controllers/project.controllers.js
@@ -1,8 +1,18 @@
 import { response, request } from 'express'
-import { Project } from '../models/project.models.js'
-import { Setting } from '../models/setting.models.js'
-import { Vector } from '../models/vector.models.js'
-import { User } from '../models/user.models.js'
+import {
+  Project,
+  Setting,
+  Vector,
+  User,
+  Activity,
+  Area,
+  Criteria,
+  Equip_Vector_Value,
+  Operational_Streets,
+  Operational_Streets_Values,
+  SubArea,
+  ValueVector
+} from '../models'
 
 export const projectPost = async (req = request, res = response) => {
   const { project_name, user_id, unit, leakage, value_leakage, period } =
@@ -51,8 +61,10 @@ export const projectGet = async (req = request, res = response) => {
   try {
     const project = await Project.findAll({
       include: [
-        { model: User, 
-          attributes: ['user_name'] },
+        {
+          model: User,
+          attributes: ['user_name']
+        },
         {
           model: Vector,
           attributes: ['id']
@@ -88,5 +100,133 @@ export const projectDelete = async (req = request, res = response) => {
     res.status(200).json({ msg: 'project deleted correctly', projects })
   } catch (error) {
     res.status(500).json({ msg: 'error deleting project' })
+  }
+}
+
+export const importProject = async (req = request, res = response) => {
+  const {
+    project_name,
+    user_id,
+    unit,
+    leakage,
+    value_leakage,
+    period,
+    area,
+    subArea,
+    activity,
+    criteria,
+  } = req.body;
+
+  try {
+    const project = {
+      project_name: project_name,
+      user_id: user_id
+    }
+    const addProject = Project.build(project)
+    const projectSaved = await addProject.save()
+    const newSetting = {
+      unit: unit,
+      leakage: leakage,
+      value_leakage: value_leakage,
+      period: period,
+      // project_id: addProject.dataValues.id
+      project_id: projectSaved.id
+    }
+
+    const addSetting = Setting.build(newSetting)
+    await addSetting.save()
+
+    const addArea = Area.build(area)
+    const areaSaved = await addArea.save()
+
+    const addSubArea = SubArea.build(subArea)
+    const subAreaSaved = await addSubArea.save()
+
+    const addActivity = Activity.build(activity)
+    const activitySaved = await addActivity.save()
+
+    for (const crit of criteria) {
+      const newCriteria = {
+        project_id: projectSaved.id,
+        name: crit.name,
+        value: crit.value,
+        type_vector: crit?.type_vector,
+        other_vector: crit?.other_vector
+      }
+      const addCriteria = Criteria.build(newCriteria)
+      const criteriaSaved = await addCriteria.save()
+
+      for (const vect of crit.vector) {
+        const newVector = {
+          user_id: user_id,
+          area_id: areaSaved.id,
+          sub_area_id: subAreaSaved.id,
+          activity_id: activitySaved.id,
+          criteria_id: criteriaSaved.id,
+          project_id: projectSaved.id,
+          availability: vect.availability,
+          air_velocity: vect.air_velocity || 0,
+          area_m2: vect.area_m2 || 0,
+          fix_q: vect.fix_q || 0,
+          intake_t: vect.intake_t || 0,
+          output_t: vect.output_t || 0,
+          k_w: vect.k_w || 0,
+          r_h: vect.r_h || 0,
+          volume_m3: vect.volume_m3 || 0,
+          vector: vect.vector,
+          power_input: vect.power_input,
+          type_vector: vect.type_vector,
+          position: vect.position,
+        }
+        const addVector = Vector.build(newVector)
+        const vectorSaved = await addVector.save()
+
+        const newOperational_Streets = {
+          ...vect.Operational_Streets,
+          vector_id: vectorSaved.id,
+          criteria_id: criteriaSaved.id,
+        };
+        const addOperational_Streets = Operational_Streets.build(newOperational_Streets)
+        await addOperational_Streets.save()
+
+        const newEquip_Vector_Value = {
+          ...vect.Equip_Vector_Value,
+          vector_id: vectorSaved.id,
+        }
+        const addEquip_Vector_Value = Equip_Vector_Value.build(newEquip_Vector_Value)
+        await addEquip_Vector_Value.save()
+
+        const newOperational_Streets_Values = {
+          ...vect.Operational_Streets_Values,
+          vector_id: vectorSaved.id,
+        }
+        const addOperational_Streets_Values = Operational_Streets_Values.build(newOperational_Streets_Values)
+        await addOperational_Streets_Values.save()
+
+        const newValueVector = {
+          ...ValueVector,
+          user_id: user_id,
+          vector_id: vectorSaved.id,
+        }
+        const addValueVector = ValueVector.build(newValueVector)
+        await addValueVector.save()
+      }
+    }
+
+    const projects = await Project.findAll({
+      include: [
+        { model: User, attributes: ['user_name'] },
+        {
+          model: Vector,
+          attributes: ['id']
+        }
+      ],
+      attributes: { exclude: ['user_id'] },
+      where: { user_id: user_id }
+    })
+
+    res.status(201).json({ msg: 'project created correctly', projects })
+  } catch (error) {
+    res.status(500).json({ msg: 'error creating project' })
   }
 }
--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import {
+  importProject,
   projectDelete,
   projectGet,
   projectPost
@@ -10,4 +11,21 @@ export const projectRt = Router()
 
 projectRt.get('/projects/:user_id', projectGet)
 projectRt.post('/newProject', [fieldValidation], projectPost)
+projectRt.post(
+  '/exportProject', 
+  [ 
+    check('project_name','El project_name es obligatorio').not().isEmpty(),
+    check('user_id','El user_id es obligatorio').not().isEmpty(),
+    check('unit','El unit es obligatorio').not().isEmpty(),
+    check('leakage','El leakage es obligatorio').not().isEmpty(),
+    check('value_leakage','El value_leakage es obligatorio').not().isEmpty(),
+    check('period','El period es obligatorio').not().isEmpty(),
+    check('area','El area es obligatorio').not().isEmpty(),
+    check('subArea','El subArea es obligatorio').not().isEmpty(),
+    check('activity','El activity es obligatorio').not().isEmpty(),
+    check('criteria','El criteria es obligatorio').not().isEmpty(),
+    fieldValidation
+  ],
+  importProject
+)
 projectRt.delete('/deleteProject/:id/:user_id', projectDelete)
